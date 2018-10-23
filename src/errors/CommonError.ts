82789556f38c4ed17f186b25af760c54a31ab1ae
derushio <narumi18wa@gmail.com>
2018-10-23 18:51:23 +0900

    <master>[modified] update errors

--- src/errors/CommonError.ts
+++ src/errors/CommonError.ts
@@ -1,4 +1,4 @@
-import ExtendableError from '@/model/error/ExtendableError';
+import ExtendableError from '@/errors/ExtendableError';
 
 export class CommonError extends ExtendableError<any> {
     public constructor(message: string, extra: any = {}) {
