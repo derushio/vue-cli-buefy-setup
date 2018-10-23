7531c9b6fdfbf9e6e4a11e52d8615f0f70ad1f45
derushio <narumi18wa@gmail.com>
2018-10-23 14:04:30 +0900

    <master>[modified] update ConsoleUtil

--- src/utils/ConsoleUtil.ts
+++ src/utils/ConsoleUtil.ts
@@ -8,33 +8,33 @@ export default class ConsoleUtil {
     public static consoleLevel = (process.env.NODE_ENV === 'develop')
         ? ConsoleLevel.info : ConsoleLevel.error;
 
-    public static info(message?: any, ...optionalParams: any[]) {
+    public static info(message?: any, ...optionalParams: any) {
         if (this.consoleLevel <= ConsoleLevel.info) {
-            console.info(message, optionalParams);
+            console.info(message, ...optionalParams);
         }
     }
 
-    public static log(message?: any, ...optionalParams: any[]) {
+    public static log(message?: any, ...optionalParams: any) {
         if (this.consoleLevel <= ConsoleLevel.log) {
-            console.log(message, optionalParams);
+            console.log(message, ...optionalParams);
         }
     }
 
-    public static table(tabularData: any, properties?: string[]) {
+    public static table(tabularData: any, properties?: string) {
         if (this.consoleLevel <= ConsoleLevel.log) {
             console.table(tabularData, properties);
         }
     }
 
-    public static assert(value?: any, message?: string, ...optionalParams: any[]) {
+    public static assert(value?: any, message?: string, ...optionalParams: any) {
         if (this.consoleLevel <= ConsoleLevel.assert) {
-            console.assert(value, message, optionalParams);
+            console.assert(value, message, ...optionalParams);
         }
     }
 
-    public static error(message?: any, ...optionalParams: any[]) {
+    public static error(message?: any, ...optionalParams: any) {
         if (this.consoleLevel <= ConsoleLevel.error) {
-            console.error(message, optionalParams);
+            console.error(message, ...optionalParams);
         }
     }
 }
